9490396ffd60db9930a1489115ce862576441d8c
SahilJangid2611 <105125368+SahilJangid2611@users.noreply.github.com>
2025-01-03 22:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+    const prices = document.querySelectorAll('.price');
+    let total = 0;
+
+    prices.forEach(price => {
+        total += parseInt(price.textContent);
+    });
+
+    const totalRow = document.createElement('tr');
+    const totalCell = document.createElement('td');
+    totalCell.setAttribute('colspan', '2');
+    totalCell.textContent = 'Total Price: Rs ' + total;
+
+    totalRow.appendChild(totalCell);
+    document.querySelector('table').appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
